443157d5bb5c182a1fe1f272ac5908669cf11310
Oliver <christof.cline@g.austincc.edu>
2023-01-17 14:29:43 -0600

    create Footer component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Head from './Head';
 import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <Head/>
       <Header/>
+      <Footer/>
     </div>
   );
 }
