0bfbd88eae9fe54943d08c0e2ea1b730956f9ad6
Oliver <christof.cline@g.austincc.edu>
2023-01-23 11:02:26 -0600

    add Profile.js component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Login from './components/Login';
+import Profile from './components/Profile';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Login />
+      <Profile />
       <Footer />
     </div>
   );
