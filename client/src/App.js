f55d8b77700cc9b450456123d5cf3ab8e60bd830
Oliver <christof.cline@g.austincc.edu>
2023-01-23 11:14:58 -0600

    add ArticleCreation.js component

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Login from './components/Login';
 import Profile from './components/Profile';
+import Settings from './components/Settings';
+import ArticleCreation from './components/ArticleCreation';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Profile />
+      <ArticleCreation />
       <Footer />
     </div>
   );
