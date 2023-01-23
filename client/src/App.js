d029f2a02761ca5bc559630ea0a453a6458ac725
Oliver <christof.cline@g.austincc.edu>
2023-01-23 11:00:34 -0600

    add Login.js component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Login from './components/Login';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Login />
       <Footer />
     </div>
   );
